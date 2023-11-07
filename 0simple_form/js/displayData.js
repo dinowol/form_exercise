9a01e13df53285bbeb316338c944f90821812a05
DiNowack <dianne.nowack@maibornwolff.de>
2023-11-07 13:27:30 +0100

    delete check

--- 0simple_form/js/displayData.js
+++ 0simple_form/js/displayData.js
@@ -4,7 +4,6 @@ var docRef = db.collection("contacts"); // Reference of the "document", thus the
 docRef
   .get()
   .then((data) => {
-    // if (data && data.length > 0) {
     data.forEach((doc) => {
       console.log(doc.id, " => ", doc.data());
       const item = doc.data();
@@ -12,11 +11,6 @@ docRef
       listElement.textContent = `Name: ${item.name}, Email: ${item.email}`;
       dataList.appendChild(listElement);
     });
-    // } else { //TODO: check
-    //   const noDataText = document.createElement("p");
-    //   noDataText.textContent = "No data available.";
-    //   dataList.appendChild(noDataText);
-    // }
   })
   .catch((error) => {
     console.log("Error getting document:", error);
